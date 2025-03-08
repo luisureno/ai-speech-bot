1d9489be8a341c2caef444ec5735b16dd76a4bfb
Luis Angel Ureno <lureno2@horizon.csueastbay.edu>
2025-03-07 20:14:47 -0800

    added api but still in debug mode

--- index.js
+++ index.js
@@ -10,8 +10,28 @@ app.get('/', (req, res) => {
     res.sendFile('index.html'); //sends the html file to the client
 });
 
+const apiai = require('apiai')('4186267be6e82cfd0b15417cf03aa36695e7e496');
 
 
-
-
-
+io.on('connection', function(socket) {
+    socket.on('chat message', (text) => {
+  
+      // Get a reply from API.AI
+  
+      let apiaiReq = apiai.textRequest(text, {
+        sessionId: APIAI_SESSION_ID
+      });
+  
+      apiaiReq.on('response', (response) => {
+        let aiText = response.result.fulfillment.speech;
+        socket.emit('bot reply', aiText); // Send the result back to the browser!
+      });
+  
+      apiaiReq.on('error', (error) => {
+        console.log(error);
+      });
+  
+      apiaiReq.end();
+  
+    });
+  });
--- public/js/script.js
+++ public/js/script.js
@@ -10,3 +10,32 @@ const recognition = new SpeechRecognition();
 document.querySelector('buttoon').addEventListener('click', () => {
     recognition.start(); //starts the recognition engine    
 });
+
+//'result' event occurs when speech is detected and processed
+recognition.addEventListener('result', (e) => {
+    //the speech recognition engine returns different parts of a sentence, so
+    //this line will get the last part of the sentence since its more accurate
+    let last = e.results.length - 1; 
+    //retreives the transcipt from the last result
+    let text = e.results[last][0].transcipt;
+    //confidence score of the speech recognition engine between 0 and 1
+    console.log('Confidence: ' + e.results[0][0].confidence);
+});
+
+const socket = io(); //connects to the server  
+
+socket.emit('chat message', text); //sends the text to the server
+
+function synthVoice(text) {
+    const synth = window.speechSynthesis;
+    const utterance = new SpeechSynthesisUtterance();
+    utterance.text = text;
+    synth.speak(utterance);
+  }
+
+SpeechSynthesis.speak()
+
+socket.on('bot reply', function(replyText) {
+    synthVoice(replyText);
+  });
+  
